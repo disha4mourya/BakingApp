d0414916a6308e72cd7091d57f6b8e9dafcb7e47
Disha Mourya <disha4mourya@gmail.com>
2018-08-20 15:22:17 +0530

    details view started with exoplayer

--- app/src/main/java/com/example/bakinapp/RecipeDetails/fragment/MasterListFragment.java
+++ app/src/main/java/com/example/bakinapp/RecipeDetails/fragment/MasterListFragment.java
@@ -1,4 +1,26 @@
 package com.example.bakinapp.RecipeDetails.fragment;
 
-public class MasterListFragment {
+import android.app.Fragment;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.imerchantech.bakinapp.R;
+import com.imerchantech.bakinapp.databinding.FragmentMasterListBinding;
+
+public class MasterListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        FragmentMasterListBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_master_list, container, false);
+        View view = binding.getRoot();
+        //here data must be an instance of the class MarsDataProvider
+
+        return view;
+    }
 }
